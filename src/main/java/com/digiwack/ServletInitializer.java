e62c9186661116a5bab30299740376c22e27cd11
giannileuani <gianni.leuani@gmail.com>
2020-03-02 04:05:45 -0600

    Comment at top

--- src/main/java/com/digiwack/ServletInitializer.java
+++ src/main/java/com/digiwack/ServletInitializer.java
@@ -2,7 +2,9 @@ package com.digiwack;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+/**
+*Noting too special here, SpringBoot plugin for Eclipse generates this file
+*/
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
