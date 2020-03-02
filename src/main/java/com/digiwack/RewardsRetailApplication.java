75ec2d6a93589f8affaf95214ff1780e9a43beed
giannileuani <gianni.leuani@gmail.com>
2020-03-02 04:04:56 -0600

    Comment on top

--- src/main/java/com/digiwack/RewardsRetailApplication.java
+++ src/main/java/com/digiwack/RewardsRetailApplication.java
@@ -3,6 +3,9 @@ package com.digiwack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+*Nothing too special here, SpringBoot plugin for Eclipse makes this file by default
+*/
 @SpringBootApplication
 public class RewardsRetailApplication {
 
